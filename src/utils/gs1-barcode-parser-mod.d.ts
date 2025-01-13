e2ef8a78abf6cf8e17526f418afc055589d09727
Gerald-Waerseggers <geraldwae@gmail.com>
2025-01-13 14:39:24 +0100

    Update gs1-barcode-parser-mod.d.ts

--- src/utils/gs1-barcode-parser-mod.d.ts
+++ src/utils/gs1-barcode-parser-mod.d.ts
@@ -1,11 +1,11 @@
-declare module 'gs1-barcode-parser-mod' {
-    export class GS1BarcodeParser {
-      parse(barcode: string): {
-        gtin?: string;
-        batchNumber?: string;
-        expirationDate?: string;
-        serialNumber?: string;
-        [key: string]: string | undefined;
-      };
-    }
+declare module "gs1-barcode-parser-mod" {
+  export class GS1BarcodeParser {
+    parse(barcode: string): {
+      gtin?: string;
+      batchNumber?: string;
+      expirationDate?: string;
+      serialNumber?: string;
+      [key: string]: string | undefined;
+    };
   }
+}
